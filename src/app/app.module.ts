cf913836db91db64499c140a494a4d1a8a7e6c43
Adriano da Silva de Jesus <adriano.jesus@capgemini.com>
2018-09-10 18:05:58 -0300

    Ajustes na chamda de ususario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { PayPal } from '@ionic-native/paypal';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
@@ -49,6 +50,7 @@ const FIREBASE_OPTIONS: FirebaseOptions = {
     ScreenOrientation,
     Dialog,
     PaypalProvider,
+    PayPal,
     AuthProvider,
     DatabaseProvider
   ]
